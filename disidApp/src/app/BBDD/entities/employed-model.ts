5ef8fed7699531eeaa79e3779d6520d71ca58cfc
Angel Banegas <mcinegas@gmail.com>
2020-08-02 18:45:12 +0200

    Entities model defined

--- disidApp/src/app/BBDD/entities/employed-model.ts
+++ disidApp/src/app/BBDD/entities/employed-model.ts
@@ -1,7 +1,14 @@
 import {Long} from 'long';
 
+export class Departament {
+    public id:Long;
+    public name:string;
+}
+
 export class Employed {
     public id:Long;
     public name:string;
-
+    public lastName:string;
+    public age:number;
+    public entryDate:Date = new Date();
 }
